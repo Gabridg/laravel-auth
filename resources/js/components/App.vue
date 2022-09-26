0af004095e8d33285c258907b5e3fac9f7ba58b6
Gabridg <gabri99.del@gmail.com>
2022-09-26 19:56:40 +0200

    connected vue, styled home & started using vue and scss

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="title m-b-md">
+        Coming soon...
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+}
+</script>
+
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function (){
-    return view('guest.home');
-});
-
 Auth::routes();
 
-Route::get('/admin', 'HomeController@index')->name('admin.home');
+
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
+    Route::get('/', 'HomeController@index')->middleware('auth')->name('home');
+    
+    
+    // Route::resource('posts', 'PostController');
+
+    Route::get('/{any}', function(){
+        abort('404');
+    })->where('any', '.*');
+});
+
+Route::get('/{any?}', function (){
+    return view('guest.home');
+})->where('any', '.*');
