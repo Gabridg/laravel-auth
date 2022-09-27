95cce21618a4d9997f020ce7ca8e65136ba978f0
Gabridg <gabri99.del@gmail.com>
2022-09-27 16:45:13 +0200

    preparing CRUD show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'DESC')->orderBy('updated_at', 'DESC')->get();
 
         return view('admin.posts.index', compact('posts'));
     }
@@ -50,7 +50,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        
     }
 
     /**
