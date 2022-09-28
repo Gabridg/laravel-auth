afdad143b16cded868111e99ced8283c4a4dc894
Gabridg <gabri99.del@gmail.com>
2022-09-28 18:30:46 +0200

    added migration & seed of category & connection

---
+++ config/categories.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    ['label'=> 'HTML', 'color'=> 'danger'],
+    ['label'=> 'CSS', 'color'=> 'primary'],
+    ['label'=> 'PHP', 'color'=> 'warning'],
+    ['label'=> 'JavaScript', 'color'=> 'orange'],
+    ['label'=> 'VueJS', 'color'=> 'success'],
+    ['label'=> 'Laravel', 'color'=> 'secondary'],
+    ['label'=> 'MySQL', 'color'=> 'purple'],
+];
